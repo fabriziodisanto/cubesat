397e20d265880e3c749af294887a05a39b8aa06e
lourdes pontiroli <pontiroli.lourdes@gmail.com>
2023-07-28 03:07:36 -0300

    tuki

--- src/components/card.js
+++ src/components/card.js
@@ -3,7 +3,7 @@ export default function Card({title,description,src}){
     return(
 
         <div className="cardcontainer">
-            <img src={src}/>
+            <img src={src} alt="img-slide"/>
             <div className="cardtext">
                 <h4>{title}</h4>
                 <p>{description}</p>
