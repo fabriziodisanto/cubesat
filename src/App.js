c9e17998e2067227658cd9c8f64352369b6a74e8
lourdes pontiroli <pontiroli.lourdes@gmail.com>
2023-08-09 14:57:35 -0300

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 
-import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/navbar';
-import AboutUs from './pages/aboutus';
-import Contact from './pages/contacto';
-import PanelesSolares from './pages/panelessolares';
 import Whole from './pages/whole';
 
 function App() {
@@ -14,11 +11,7 @@ function App() {
       <BrowserRouter>
       <Navbar />
         <Routes>
-          <Route exact path='/' element={<Navigate replace to ={'/nosotros'}/>}/>
-          <Route exact path="/nosotros" element={<AboutUs />} />
-          <Route exact path='/contacto' element={<Contact />}/>
-          <Route exact path='/panelessolares' element={<PanelesSolares />}/>
-          <Route exact path='/whole' element={<Whole/>}/>
+          <Route exact path='/' element={<Whole/>}/>
         </Routes>
       </BrowserRouter>
 
