26481e4c106c8b5b6c6f81f63894f8e27678a39f
Lourdes Pontiroli <pontiroli.lourdes@gmail.com>
2023-08-09 21:49:11 -0300

    fix

--- src/pages/whole.js
+++ src/pages/whole.js
@@ -3,6 +3,7 @@ import img1 from '../img/img1.jpg'
 import car2 from '../img/car2.webp'
 import carou from '../img/carou.JPG'
 import logo from '../img/logosnl_inverted.jpg'
+import imgor from '../img/4tocar.jpg'
 import { Carousel } from "react-bootstrap";
 
 export default function Whole() {
@@ -19,7 +20,10 @@ export default function Whole() {
                         <Card title={'Paneles Solares Argentinos: Calidad Asegurada'} description={'Los paneles que ofrecemos son integrados en laboratorios de nuestro país donde se fabricaron todos los paneles solares acoplados a los satélites construidos en la República Argentina, bajo estricto cumplimiento de todos los ensayos de aceptación y calificación de las Normas Vigentes.'} src={img1} />
                     </Carousel.Item>
                     <Carousel.Item>
-                        <Card title={'Procedimientos Certificados para Paneles Solares'} description={'La integración de las celdas a los paneles se realiza con los mismos procedimientos aprobados por los organismos internacionales, con realización de ciclados térmicos en vacío y en atmósfera controlada. Además, a cada panel se le realizan los informes de Aislamiento de continuidad entre celdas y sustrato y verificaciones de funcionamiento, Flash e inspecciones visuales de las celdas solares.'} src={car2} />
+                        <Card title={'Producto Fiel'} description={'A cada panel se le realizan los informes de Aislamiento de continuidad entre celdas y sustrato y verificaciones de funcionamiento, Flash e inspecciones visuales de las celdas solares.'} src={imgor} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <Card title={'Procedimientos Certificados para Paneles Solares'} description={'La integración de las celdas a los paneles se realiza con los mismos procedimientos aprobados por los organismos internacionales, con realización de ciclados térmicos en vacío y en atmósfera controlada.'} src={car2} />
 
                     </Carousel.Item>
                     <Carousel.Item>
